c6b6e6650aaf44f2523c69f9e04898d0214d448b
Konstantin Dryagin <gunster98@gmail.com>
2025-07-10 11:46:07 +0300

    fix: удалил лишний путь

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
-      "@app": resolve(__dirname, "src/app"),
       "@assets": resolve(__dirname, "src/assets"),
       "@entities": resolve(__dirname, "src/entities"),
       "@features": resolve(__dirname, "src/features"),
